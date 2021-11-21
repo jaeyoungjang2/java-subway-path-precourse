8fae654ce9ebf637066653754f8ed819e2b180bf
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 13:05:12 +0900

    test: 역간 이동시 걸리는 시간에 대한 테스트 걸리는 시간이 1분 보다 작을 경우 예외 처리 테스트 git push git status

--- src/test/java/subway/domain/TimeTest.java
+++ src/test/java/subway/domain/TimeTest.java
@@ -1,4 +1,18 @@
+package subway.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import subway.exception.MinimumTimeException;
+
 class TimeTest {
-  
+    @DisplayName("역간 이동에 걸리는 최소시간은 1초이다. 0초일 경우에는 에러가 발생해야 한다.")
+    @Test
+    void illegalTimeTest() {
+        Assertions.assertThatThrownBy(() -> {
+            new Time(0);
+        }).isInstanceOf(MinimumTimeException.class);
+    }
 }
