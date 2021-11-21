10a40cb2e9a1f02821bbf73f2a30f6cef1252973
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 20:35:45 +0900

    feat: 역간 이동한 거리 정보 저장

--- src/main/java/subway/domain/Station2StationDistanceRepository.java
+++ src/main/java/subway/domain/Station2StationDistanceRepository.java
@@ -1,2 +1,22 @@
-package subway.domain;public class Station2StationDistanceRepository {
+package subway.domain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Station2StationDistanceRepository {
+    Map<Station, Map<Station, Distance>> distanceMap;
+
+    public Station2StationDistanceRepository() {
+        this.distanceMap = new HashMap<>();
+    }
+
+    public void add(Station startStation, Station endStation, Distance distance) {
+        distanceMap.put(startStation, new HashMap<>());
+        distanceMap.get(startStation).put(endStation, distance);
+    }
+
+    public Map<Station, Map<Station, Distance>> info() {
+        return Collections.unmodifiableMap(distanceMap);
+    }
 }
