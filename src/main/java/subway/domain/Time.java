3e95dbf2c82636e2ceae0965e47642fcbc8cf16c
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 13:03:47 +0900

    test: 역간 이동시 걸리는 시간에 대한 테스트 걸리는 시간이 1분 보다 작을 경우 예외 처리

--- src/main/java/subway/domain/Time.java
+++ src/main/java/subway/domain/Time.java
@@ -1,2 +1,19 @@
-package subway.domain;public class Time {
+package subway.domain;
+
+import subway.exception.MinimumTimeException;
+
+public class Time {
+    private static final Integer minimum = 1;
+    private final Integer time;
+
+    public Time(Integer time) {
+        validTime(time);
+        this.time = time;
+    }
+
+    private void validTime(Integer time) {
+        if (time < minimum) {
+            throw new MinimumTimeException();
+        }
+    }
 }
