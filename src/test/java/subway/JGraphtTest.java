1fa186418a234e772d521cdf44eb120346b9a7d5
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 13:40:34 +0900

    test: 다익스트라 계산 테스트

--- src/test/java/subway/JGraphtTest.java
+++ src/test/java/subway/JGraphtTest.java
@@ -3,15 +3,18 @@ package subway;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class JGraphtTest {
+    @DisplayName("최소 거리를 가기 위한 역의 개수 확인")
     @Test
-    public void getDijkstraShortestPath() {
+    public void getDijkstraShortestPath1() {
         WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
         graph.addVertex("v1");
         graph.addVertex("v2");
@@ -24,5 +27,57 @@ public class JGraphtTest {
         List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
 
         assertThat(shortestPath.size()).isEqualTo(3);
+    }
+
+    @DisplayName("최소 거리를 가기 위한 거치는 역의 이름 확인")
+    @Test
+    public void getDijkstraShortestPath2() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+
+        assertThat(shortestPath.get(0)).isEqualTo("v3");
+        assertThat(shortestPath.get(1)).isEqualTo("v2");
+        assertThat(shortestPath.get(2)).isEqualTo("v1");
+    }
+
+    @DisplayName("역과 역 사이를 이동할 수 없는 경우 에러 확인")
+    @Test
+    public void getDijkstraShortestPath3() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+
+        assertThatThrownBy(() -> {
+            dijkstraShortestPath.getPath("v3", "v4").getVertexList();
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("역과 역 사이를 이동할 수 없는 경우 에러 확인")
+    @Test
+    public void getDijkstraShortestPath4() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        double weight = dijkstraShortestPath.getPath("v3", "v1").getWeight();
     }
 }
