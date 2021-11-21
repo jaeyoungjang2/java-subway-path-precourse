ce52299907fec540ad240ec5693d134b1147dc29
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 13:04:33 +0900

    feat: 역간 이동시 걸리는 시간에 대한 예외처리 추가 역간 이동시 걸리는 시간이 1분 보다 작을 경우 예외 처리 발생

--- src/main/java/subway/exception/MinimumTimeException.java
+++ src/main/java/subway/exception/MinimumTimeException.java
@@ -1,2 +1,10 @@
-package subway.exception;public class MinimumTimeException {
+package subway.exception;
+
+public class MinimumTimeException extends IllegalArgumentException {
+
+    private static String MINIMUM_TIME_EXCEPTION_MESSAGE = "역간 이동에 걸리는 최소시간은 1분 입니다. (1분 보다 작은 수는 존재할 수 없습니다.)";
+
+    public MinimumTimeException() {
+        super(MINIMUM_TIME_EXCEPTION_MESSAGE);
+    }
 }
