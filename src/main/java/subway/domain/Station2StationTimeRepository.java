8f0900ab85c926e9ccd30f08bce7b4d81a136aff
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 20:36:33 +0900

    feat: 역간 이동서 걸리는 시간 정보 저장

--- src/main/java/subway/domain/Station2StationTimeRepository.java
+++ src/main/java/subway/domain/Station2StationTimeRepository.java
@@ -1,2 +1,23 @@
-package subway.domain;public class Station2StationTimeRepository {
+package subway.domain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Station2StationTimeRepository {
+
+    Map<Station, Map<Station, Time>> timeMap;
+
+    public Station2StationTimeRepository() {
+        this.timeMap = new HashMap<>();
+    }
+
+    public void add(Station startStation, Station endStation, Time time) {
+        timeMap.put(startStation, new HashMap<>());
+        timeMap.get(startStation).put(endStation, time);
+    }
+
+    public Map<Station, Map<Station, Time>> info() {
+        return Collections.unmodifiableMap(timeMap);
+    }
 }
