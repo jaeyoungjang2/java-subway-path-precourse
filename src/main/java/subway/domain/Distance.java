557275ad2b4252f2731923f6aa35cb18ddfdf3f7
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 20:06:51 +0900

    feat: 역간 거리에 대한 클래스 추가

--- src/main/java/subway/domain/Distance.java
+++ src/main/java/subway/domain/Distance.java
@@ -1,2 +1,23 @@
-package subway.domain;public class Distance {
+package subway.domain;
+
+import subway.exception.MinimumDistanceException;
+
+public class Distance {
+    private static final Integer minimum = 1;
+    private final Integer distance;
+
+    public Distance(Integer distance) {
+        validTime(distance);
+        this.distance = distance;
+    }
+
+    private void validTime(Integer distance) {
+        if (distance < minimum) {
+            throw new MinimumDistanceException();
+        }
+    }
+
+    public Integer info() {
+        return distance;
+    }
 }
