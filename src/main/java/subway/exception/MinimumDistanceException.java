5ea664a85ba6ea9f04123d0811a1a58dacd74275
jaeyoungjang2 <lion85488@gmail.com>
2021-11-21 20:36:12 +0900

    test: 역간 이동시 걸리는 시간에 대한 예외처리

--- src/main/java/subway/exception/MinimumDistanceException.java
+++ src/main/java/subway/exception/MinimumDistanceException.java
@@ -1,2 +1,9 @@
-package subway.domain;public class MinimumDistanceException {
+package subway.exception;
+
+public class MinimumDistanceException extends IllegalArgumentException {
+    private static String MINIMUM_DISTANCE_EXCEPTION_MESSAGE = "역간 최소 거리는 1km 입니다. (1km 보다 작은 수는 존재할 수 없습니다.)";
+
+    public MinimumDistanceException() {
+        super(MINIMUM_DISTANCE_EXCEPTION_MESSAGE);
+    }
 }
